267b4e147ddffc774faa2983652e1a3b52ead88e
Alan Matkorski <alanmatkorski@gmail.com>
2017-03-22 18:38:26 -0300

    Maximo

--- app/Telegram/UpdateHandlers/ResponseByMatch.php
+++ app/Telegram/UpdateHandlers/ResponseByMatch.php
@@ -8,6 +8,7 @@ class ResponseByMatch implements UpdateHandler
     private $mappings = [
         '/macri/i' => 'Gato',
         '/marcos/i' => '¿Quisiste decir Markos?',
+        '/maximo/i' => 'Para programarme no usaron ni un solo if ;)',
         '/facu/i'  => 'Facu... ese tipo es medio puto',
         '/dan/i'  => 'ese tiene tatuado pattern matching en el culo!'
     ];
