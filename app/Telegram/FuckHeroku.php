fb448a33423f8fe268e228821fb1d292f7a744ad
danwyry <danwyry@gmail.com>
2017-03-22 20:53:57 -0300

    Fucking antiheroku logger

--- app/Telegram/FuckHeroku.php
+++ app/Telegram/FuckHeroku.php
@@ -4,9 +4,14 @@ class FuckHeroku
 {
 	public static function log($loggable)
 	{
-		$file = dirname(__FILE__) . '/fuckheroku.log'; 
+		$file = app_path('Telegram/fuckheroku.log'); 
 		$data = self::format_loggable($loggable); 
 		file_put_contents($file, $data, FILE_APPEND);
+	}
+	public static function get_log()
+	{
+		$file = app_path('Telegram/fuckheroku.log'); 
+		return file_get_contents($file); 
 	}
 
 	private function format_loggable($loggable)
@@ -19,9 +24,4 @@ class FuckHeroku
 		}
 	}
 
-	public static function get_log()
-	{
-		$file = dirname(__FILE__) . '/fuckheroku.log'; 
-		return file_get_contents($file); 
-	}
 }
