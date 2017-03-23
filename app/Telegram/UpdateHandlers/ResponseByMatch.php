bcbe3a20c5aa0a6fa21e8313d433e44e4c4cff4a
danwyry <danwyry@gmail.com>
2017-03-22 21:42:29 -0300

    Fucking antiheroku logger

--- app/Telegram/UpdateHandlers/ResponseByMatch.php
+++ app/Telegram/UpdateHandlers/ResponseByMatch.php
@@ -15,6 +15,10 @@ class ResponseByMatch implements UpdateHandler
 
     public function shouldHandle($updates, $context=null) 
     {
+        // me aseguro que el request que vino trae los datos que quiero... 
+        if ( ! (isset($updates->message) && isset($updates->message->text)) )
+            return false ; 
+
         foreach ($this->mappings as $needle => $response)
             if ( preg_match($needle, $updates->message->text) )
                 return true ; 
