f429a82ebec74496e42e5e9eef652f2acbd5e226
danwyry <danwyry@gmail.com>
2017-03-22 22:33:16 -0300

    agrego mappings al response by match

--- config/espinoso_data.php
+++ config/espinoso_data.php
@@ -5,9 +5,10 @@ $rbmMappings =  [
             '/maximo/i' => 'Para programarme no usaron ni un solo if ;)',
             '/facu/i'  => 'Facu... ese tipo es medio puto',
             '/dan/i'  => 'ese tiene tatuado pattern matching en el culo!',
+            '/maldici[oó]n/i'  => 'tranquilo vieja, todo va a salir bien.',
         ];
 
-$rbmMappings['/^claves?$/'] = implode(', ', array_keys($rbmMappings));
+$rbmMappings['/espinoso,[]*claves\?/i'] = implode(', ', array_keys($rbmMappings));
 
 return [
     'ResponseByMatch' => [ 'mappings' => $rbmMappings ], 
