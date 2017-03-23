3db3f8cccbc61dd265ac964a16e9882ecffa7b97
danwyryunq <dandanielw@gmail.com>
2017-03-23 13:13:06 -0300

    ooops, corrección de los handlers registrados

--- config/espinoso.php
+++ config/espinoso.php
@@ -2,10 +2,9 @@
 return [
 
     'handlers' => [ 
-        App\Espinoso\Handlers\ResponseByMatch::class
-        App\Espinoso\Handlers\BardoDelEspinoso::class
-        App\Espinoso\Handlers\RandomInstagram::class
-        App\Espinoso\Handlers\ResponseByMatch::class
+        App\Espinoso\Handlers\ResponseByMatch::class,
+        App\Espinoso\Handlers\BardoDelEspinoso::class,
+        App\Espinoso\Handlers\RandomInstagram::class,
     ], 
 
 ];
