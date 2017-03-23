372ebf321170fc15c07ca86bbf918113a04c476f
danwyry <danwyry@gmail.com>
2017-03-22 22:46:33 -0300

    Refactor para pasar los datos de espinoso a un archivo de config.

--- config/espinoso_data.php
+++ config/espinoso_data.php
@@ -5,10 +5,16 @@ $rbmMappings =  [
             '/maximo/i' => 'Para programarme no usaron ni un solo if ;)',
             '/facu/i'  => 'Facu... ese tipo es medio puto',
             '/dan/i'  => 'ese tiene tatuado pattern matching en el culo!',
-            '/maldici[oó]n/i'  => 'tranquilo vieja, todo va a salir bien.',
+            '/maldicio[o]*n/i'  => 'tranquilo vieja, todo va a salir bien.',
+            '/fu[u]*ck/i'  => 'tranquilo vieja, todo va a salir bien.',
+            '/concha.*lora/i'  => 'no eh, cuidame la boquita.',
+            '/mamu/i'  => 'papu',
+            '/papu/i'  => 'mamu',
+            '/contrato/i'  => 'el diccionario lo define como un acuerdo legal que no se puede romper, que no se puede romper...',
+
         ];
 
-$rbmMappings['/(espinoso,)?[ ]*claves\??/i'] = implode(', ', array_keys($rbmMappings));
+$rbmMappings['/espinoso,.*claves.*?/i'] = implode(', ', array_keys($rbmMappings));
 
 return [
     'ResponseByMatch' => [ 'mappings' => $rbmMappings ], 
