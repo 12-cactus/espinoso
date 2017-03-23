f0749b8b53cabb192555e00d937ac20529ecfccd
Alan Matkorski <alanmatkorski@gmail.com>
2017-03-23 11:42:51 -0300

    Se banca las nudes?

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -12,6 +12,14 @@ class TelegramController extends Controller
         $updates = Telegram::getWebhookUpdates();
         $updates = json_decode($updates);
 
+        if($updates->message->text == 'send me nudes'){
+            return Telegram::sendPhoto([
+                'chat_id' => $updates->message->chat->id,
+                'photo' => 'https://cdn.drawception.com/images/panels/2012/4-4/FErsE1a6t7-8.png',
+                'caption' => 'Acá tenes tu nude hijo de puta!'
+            ]);
+        }
+
         Espinoso::handleTelegramUpdates($updates);
         } catch (\Exception $e)
         {
