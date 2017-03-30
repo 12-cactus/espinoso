e7d2c7740661f1676b8f4f662114f6497c3d4d11
danwyryunq <dandanielw@gmail.com>
2017-03-30 15:26:20 -0300

    borro introspection

--- config/espinoso.php
+++ config/espinoso.php
@@ -5,6 +5,5 @@ return [
         App\Espinoso\Handlers\BardoDelEspinoso::class,
         App\Espinoso\Handlers\RandomInstagram::class,
         App\Espinoso\Handlers\GoogleStaticMaps::class,
-        App\Espinoso\Handlers\Instrospection::class,
     ],
 ];
