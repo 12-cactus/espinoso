2db8e140ca6b6b5264200b46b71b7cf30d496916
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-04-06 23:46:51 -0300

    Update espinoso_data.php

--- config/espinoso_data.php
+++ config/espinoso_data.php
@@ -56,6 +56,6 @@ $rbmMappings['/espinoso.*claves.*/i'] = "Reconozco todas estas: \n" . implode("\
 
 return [
     'ResponseByMatch' => [ 'mappings' => $rbmMappings, 
-                           'ignore_names' => ['facu', 'ine', 'alan', ],
+                           'ignore_names' => ['facu', 'ine', 'alan', 'marcos'],
                          ], 
 ]; 
