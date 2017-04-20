b61b548fd2e6e386e697573f691da4f3965e6bc3
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-04-20 21:32:09 +0000

    blabla

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -37,7 +37,7 @@ class TelegramController extends Controller
         $cactus = explode(' ', $commit->author->name)[0];
 
         $message = "De nuevo el pelotudo de `$cactus` commiteando giladas, mirá lo que hizo esta vez:\n_{$commit->message}_ 
-[Link al commit]({$commit->url})";
+[Link al commit]({$commit->url}) :cactus: ";
 
         Telegram::sendMessage([
             'chat_id' => env('TELEGRAM_DEVS_CHANNEL'),
