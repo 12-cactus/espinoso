015121b2ad90b3f1b10d727e3014dc3503b69417
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-07-17 00:11:11 +0000

    github issues working?

---
+++ app/Espinoso/Handlers/GitHubHandler.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Espinoso\Handlers;
+
+use App\Espinoso\Helpers\Msg;
+use Telegram\Bot\Laravel\Facades\Telegram;
+use Telegram\Bot\HttpClients\GuzzleHttpClient;
+
+class GitHubHandler extends EspinosoHandler
+{
+    protected $matches = [];
+
+    public function shouldHandle($updates, $context = null)
+    {
+        return $this->isTextMessage($updates)
+            && preg_match('/^(espi(noso)*)(\s)(issue)(\s)(.+)$/i', $updates->message->text, $this->matches);
+    }
+
+    public function handle($updates, $context = null)
+    {
+        $title = $this->matches[6];
+        $token = config('espinoso.github.token');
+        $body = '{"title": "'.$title.'"}';
+
+        $client = new GuzzleHttpClient;
+        $response = $client->request('POST',
+            'https://api.github.com/repos/12-cactus/espinoso/issues', [
+            'Authorization' => "token {$token}",
+            'body' => $body
+        ]);
+
+        if ($response->getStatusCode() == 201) {
+            $data = json_decode($response->getBody());
+            $message = "[Issue creado!]({$data->url})";
+        } else {
+            $message = "No pude crear el issue, status ".$response->getStatusCode()."\n";
+            $message .= $response->getBody();
+        }
+        Telegram::sendMessage(Msg::md($message)->build($updates));
+    }
+}
--- config/espinoso.php
+++ config/espinoso.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'handlers' => [ 
+        App\Espinoso\Handlers\GitHubHandler::class,
         App\Espinoso\Handlers\CinemaHandler::class,
         App\Espinoso\Handlers\ResponseByMatch::class,
         App\Espinoso\Handlers\BardoDelEspinoso::class,
@@ -11,4 +12,8 @@ return [
         App\Espinoso\Handlers\ImdbHandler::class,
         App\Espinoso\Handlers\GoogleInfoBoxHandler::class,
     ],
+
+    'github' => [
+        'token' => env('GITHUB_TOKEN', '123')
+    ]
 ];
