bc22cb775dc423899848da538670180e6a20dff7
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-07-31 14:37:32 -0300

    Update espinoso.php

--- config/espinoso.php
+++ config/espinoso.php
@@ -8,7 +8,7 @@ return [
         App\Espinoso\Handlers\RandomInstagram::class,
         App\Espinoso\Handlers\GoogleStaticMaps::class,
         App\Espinoso\Handlers\Weather::class,
-        App\Espinoso\Handlers\NextHolidayHandler::class,
+        //App\Espinoso\Handlers\NextHolidayHandler::class,
         App\Espinoso\Handlers\ImdbHandler::class,
         App\Espinoso\Handlers\GoogleInfoBoxHandler::class,
     ],
