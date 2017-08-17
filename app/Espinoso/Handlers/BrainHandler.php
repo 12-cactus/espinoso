ec8c4fa71c167e4c47966d08e758e88e5cecdd1f
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2017-08-17 16:55:20 +0000

    remove comments

--- app/Espinoso/Handlers/BrainHandler.php
+++ app/Espinoso/Handlers/BrainHandler.php
@@ -52,44 +52,5 @@ class BrainHandler extends EspinosoHandler
     {
         return collect(config('brain.ignore_to'));
     }
-
-//    public function handle(Message $message)
-//    {
-//        if ($this->ignoringSender($message->getFrom())) {
-//            $fromName = $message->getFrom()->getFirstName();
-//            $msg = Msg::md("Con vos no hablo porque no viniste al asado $fromName")->build($message);
-//            $this->telegram->sendMessage($msg);
-//            return;
-//        }
-//
-//        foreach ($this->mappings() as $pattern => $response) {
-//            if ( preg_match($pattern, $message->getText()) ) {
-//                $msg = $this->buildMessage($response, $pattern, $message);
-//                $this->telegram->sendMessage($msg);
-//            }
-//        }
-//    }
-
-//    private function buildMessage($response, $pattern, Message $message)
-//    {
-//        if ($response instanceof Msg)
-//            return $response->build($message, $pattern);
-//        else
-//            return Msg::plain($response)->build($message, $pattern);
-//    }
-//
-//    private function mappings()
-//    {
-//        return config('espinoso_data.ResponseByMatch.mappings');
-//    }
-//
-
-//    private function ignoringSender($sender)
-//    {
-//        foreach ($this->ignoredNames() as $name)
-//            if ( preg_match("/$name/i", $sender->first_name) )
-//                return true ;
-//        return false ;
-//    }
     
 }
