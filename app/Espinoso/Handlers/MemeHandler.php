2f586364f329451863392a6bc5447dbccc2c4bed
Alan Matkorski <alanmatkorski@gmail.com>
2017-09-02 20:11:05 -0300

    Mejoro el MemeHandler, le falta

--- app/Espinoso/Handlers/MemeHandler.php
+++ app/Espinoso/Handlers/MemeHandler.php
@@ -3,49 +3,62 @@
 namespace App\Espinoso\Handlers;
 
 use Illuminate\Contracts\Logging\Log;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 use Telegram\Bot\Objects\Message;
 
 class MemeHandler extends EspinosoCommandHandler
 {
     protected $allow_ignore_prefix = true;
-    protected $pattern = "(?'cmd'meme)\s+((?'src'[^\s]+))\s+(\"(?'top'[^\"]+)\")\s+(\"(?'bottom'[^\"]+)\")?";
+    protected $pattern = "(?'cmd'meme)\s+((?'src'[^\s]+))\s+(\"(?'top'[^\"]+)\")(\s+(\"(?'bottom'[^\"]+)\")?)?";
 
-    protected $signature = "[espi] meme src top bottom color";
+    protected $signature = "[espi] meme src top bottom";
     protected $description = "te armo un meme y no me rompas mas las bolas";
 
     public function handle(Message $message): void
     {
-        $src = $this->matches['src'];
+        $meme = $this->generateMeme($this->matches['src'],
+            $this->matches['top'],
+            $this->matches['bottom'] ?? null);
 
-        $img = \Image::make($src);
-        $imgWidth = $img->width();
-        $imgHeight = $img->height();
+        $this->espinoso->replyImage($meme);
+    }
 
-        $x = $imgWidth / 2;
-        $y = $imgHeight - 60;
-
-
-        $top = \Illuminate\Support\Str::upper($this->matches['top']);
-        $bottom = \Illuminate\Support\Str::upper($this->matches['bottom']);
+    public function generateMeme($src, $top, $bottom = null): string
+    {
+        $fontSize = 45;
+        $fontFile = 'fonts/Impact.ttf';
         $color = '000000';
+        $storagePath = public_path() . '/img/meme.jpg';
+        $assetPath = asset('img/meme.jpg');
 
-        $img->text($top, $x, 60, function ($font) use ($color) {
-            $font->file('fonts/Impact.ttf');
-            $font->size(45);
+        $img = Image::make($src);
+        $x = $img->width() / 2;
+        $y = $img->height() - 60;
+        $textMargin = 60;
+        $top = Str::upper($top);
+        $bottom = Str::upper($bottom);
+
+        $img->text($top, $x, $textMargin, function ($font) use ($color, $fontSize, $fontFile) {
+            $font->file($fontFile);
+            $font->size($fontSize);
             $font->align('center');
             $font->valign('bottom');
             $font->color($color);
         });
 
-        $img->text($bottom, $x, $y, function ($font) use ($color) {
-            $font->file('fonts/Impact.ttf');
-            $font->size(45);
-            $font->align('center');
-            $font->valign('top');
-            $font->color($color);
-        });
+        if (!is_null($bottom)) {
+            $img->text($bottom, $x, $y, function ($font) use ($color, $fontSize, $fontFile) {
+                $font->file($fontFile);
+                $font->size($fontSize);
+                $font->align('center');
+                $font->valign('top');
+                $font->color($color);
+            });
+        }
 
-        $img->save(public_path() . '/img/meme.jpg');
-        $this->espinoso->replyImage(asset('img/meme.jpg'));
+        $img->save($storagePath);
+
+        return $assetPath;
     }
 }
