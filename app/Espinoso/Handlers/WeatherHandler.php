35f25514ea74b62dcf8c60012b9959a6682b0746
danwyryunq <dandanielw@gmail.com>
2017-11-10 12:52:00 -0300

    extiendo el weather handler con sorpresa (sin test)

--- app/Espinoso/Handlers/WeatherHandler.php
+++ app/Espinoso/Handlers/WeatherHandler.php
@@ -1,9 +1,11 @@
 <?php namespace App\Espinoso\Handlers;
 
 use Carbon\Carbon;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use App\Facades\WeatherSearch;
 use Cmfcmf\OpenWeatherMap\Forecast;
+use App\Espinoso\CronicaGenerator;
 
 class WeatherHandler extends EspinosoCommandHandler
 {
@@ -18,12 +20,16 @@ class WeatherHandler extends EspinosoCommandHandler
     {
         $date = $this->getNearestDateFromDay($this->getDay());
 
-        $weather = $this->getWeatherDescriptionForDate($date);
-
-        if (empty($weather)) {
+        $forecasts = $this->getWeatherForcastsForDate($date);
+        if ($forecasts->isEmpty()) {
             $this->replyNotFound();
         }
 
+        $cronicaTitleUrl = $this->getCronicaTitleUrlForForecast($forecasts->first());
+        if (!empty($cronicaTitleUrl))
+            $this->espinoso->reply($cronicaTitleUrl);
+
+        $weather = $this->getForecastsDescriptions($forecasts);
         $this->espinoso->reply("está pronosticado " . $weather, 'HTML');
     }
 
@@ -46,22 +52,26 @@ class WeatherHandler extends EspinosoCommandHandler
 
     protected function getNearestDateFromDay($day): Carbon
     {
-        $time = strtotime("next $day");
-
-        return Carbon::createFromFormat('U', $time);
+        return new Carbon("next $day");
     }
 
-    protected function getWeatherDescriptionForDate(Carbon $date): string
+    protected function getForecastsDescriptions(Collection $forecasts): string
+    {
+        return $forecasts
+            ->map(function (Forecast $forecast) {
+            return $this->forecastToDescription($forecast);
+        })->reduce(function ($carry, $str) {
+            return empty($carry) ? $str : $carry . "," . $str;
+        }, '');
+    }
+
+    protected function getWeatherForcastsForDate(Carbon $date) : Collection
     {
         $forecasts = WeatherSearch::getWeatherForecast('Quilmes, AR', "es", "metric", 10, '');
 
         return collect($forecasts)->filter(function (Forecast $forecast) use ($date) {
             return $this->isForecastForDate($date, $forecast);
-        })->map(function (Forecast $forecast) {
-            return $this->forecastToDescription($forecast);
-        })->reduce(function ($carry, $str) {
-            return empty($carry) ? $str : $carry . "," . $str;
-        }, '');
+        });
     }
 
     protected function isForecastForDate(Carbon $date, Forecast $forecast): bool
@@ -78,5 +88,48 @@ class WeatherHandler extends EspinosoCommandHandler
         $descrip =  $forecast->weather->description;
 
         return "de {$from} a {$to} {$descrip} con temperaturas entre {$min} y {$max} grados";
+    }
+
+    protected function getCronicaTitleUrlForForecast(Forecast $forecast)
+    {
+        $text = $this->getCronicaTitleForForecast($forecast);
+        return CronicaGenerator::getTitleUrl($text);
+    }
+
+    protected function getCronicaTitleForForecast(Forecast $forecast)
+    {
+        $titles = [
+            2 =>    "Cuidado con el rayo que te parte al medio!",
+            3 =>    "Atento el que quiere ver gotas!",
+            5 =>    "Lindo día para ponerla!",
+            8 =>    "",
+            800 =>  "Se vienen las tanguitas!",
+            801 =>  "", // few clouds
+            802 =>  "", // scattered clouds
+            803 =>  "", // broken clouds
+            804 =>  "", // overcast clouds
+            9 =>    "Guarda que se te vuela la peluca!",
+            951 => "" , // calm
+            952 => "" , // light breeze
+            953 => "" , // gentle breeze
+            954 => "" , // moderate breeze
+            955 => "" , // fresh breeze
+            956 => "" , // strong breeze
+            957 => "" , // high wind, near gale
+            958 => "" , // gale
+            959 => "" , // severe gale
+            960 => "" , // storm
+            961 => "" , // violent storm
+            962 => "" , // hurricane 
+        ];
+
+        $code = (int)$forecast->weather->id;
+        if (!array_key_exists($code, $titles))
+            $code = intdiv($code, 100);
+
+        if (!array_key_exists($code, $titles))
+            return "";
+
+        return $titles[$code];
     }
 }
