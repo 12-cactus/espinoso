35e756421eed13048eeca22f0ac9cb33ece90585
Maru <gil.maricruz@gmail.com>
2018-01-07 11:48:08 -0300

    closed #102

--- tests/Espinoso/Handlers/NextHolidaysHandlerTest.php
+++ tests/Espinoso/Handlers/NextHolidaysHandlerTest.php
@@ -1,7 +1,7 @@
 <?php namespace Tests\Espinoso\Handlers;
 
+use App\Facades\GuzzleClient;
 use Mockery;
-use App\Facades\GoutteClient;
 use Symfony\Component\DomCrawler\Crawler;
 use App\Espinoso\Handlers\NextHolidaysHandler;
 
@@ -57,37 +57,35 @@ class NextHolidaysHandlerTest extends HandlersTestCase
      */
     public function it_handle_and_return_info()
     {
-/*
-        // Mocking
-        $jsonText = "var json = '[{\"description\":\"Paso a la Inmortalidad del General Jos\u00e9 de San Mart\u00edn\",\"phrase\":\"Lunes 21 de Agosto\",\"count\":10},{\"description\":\"D\u00eda del Respeto a la Diversidad Cultural\",\"phrase\":\"Lunes 16 de Octubre\",\"count\":66},{\"description\":\"D\u00eda de la Soberan\u00eda Nacional\",\"phrase\":\"Lunes 20 de Noviembre\",\"count\":101},{\"description\":\"Inmaculada Concepci\u00f3n de Mar\u00eda\",\"phrase\":\"Viernes 08 de Diciembre\",\"count\":119},{\"description\":\"Navidad\",\"phrase\":\"Lunes 25 de Diciembre\",\"count\":136}]';
-			var position = 0;";
+
+        $jsonText = '[{"motivo":"Año Nuevo","tipo":"inamovible","dia":1,"mes":1,"id":"año-nuevo"},{"motivo":"Carnaval","tipo":"inamovible","dia":12,"mes":2,"id":"carnaval"},{"motivo":"Carnaval","tipo":"inamovible","dia":13,"mes":2,"id":"carnaval"},{"motivo":"Día Nacional de la Memoria por la Verdad y la Justicia","tipo":"inamovible","dia":24,"mes":3,"id":"memoria-verdad-justicia"},{"motivo":"Día del Veterano y de los Caídos en la Guerra de Malvinas","tipo":"inamovible","dia":2,"mes":4,"id":"veteranos-malvinas"},{"motivo":"Día del Trabajador","tipo":"inamovible","dia":1,"mes":5,"id":"trabajador"},{"motivo":"Día de la Revolución de Mayo","tipo":"inamovible","dia":25,"mes":5,"id":"revolucion-mayo"},{"motivo":"Paso a la Inmortalidad del Gral. Don Martín Güemes","tipo":"nolaborable","dia":17,"mes":6,"id":"martin-guemes"},{"motivo":"Paso a la Inmortalidad del General Manuel Belgrano","tipo":"inamovible","dia":20,"mes":6,"id":"belgrano"},{"motivo":"Día de la Independencia","tipo":"inamovible","dia":9,"mes":7,"id":"independencia"},{"motivo":"Paso a la Inmortalidad del General José de San Martín","tipo":"trasladable","original":"17-08","dia":20,"mes":8,"id":"san-martin"},{"motivo":"Día del Respeto a la Diversidad Cultural","tipo":"trasladable","original":"12-10","dia":15,"mes":10,"id":"diversidad"},{"motivo":"Día de la Soberanía Nacional","tipo":"trasladable","original":"20-11","dia":19,"mes":11,"id":"soberania-nacional"},{"motivo":"Inmaculada Concepción de María","tipo":"inamovible","dia":8,"mes":12,"id":"inmaculada-maria"},{"motivo":"Feriado Puente Turístico","tipo":"puente","dia":24,"mes":12,"id":"puente-turistico"},{"motivo":"Navidad","tipo":"inamovible","dia":25,"mes":12,"id":"navidad"},{"motivo":"Feriado Puente Turístico","tipo":"puente","dia":31,"mes":12,"id":"puente-turistico"}]';
 
         $crawler = Mockery::mock(Crawler::class);
-        $crawler->shouldReceive('filter')->with('script')->andReturnSelf();
-        $crawler->shouldReceive('eq')->with(2)->andReturnSelf();
-        $crawler->shouldReceive('text')->andReturn($jsonText);
-        GoutteClient::shouldReceive('request')
+        $crawler->shouldReceive('getBody')->andReturnSelf();
+        $crawler->shouldReceive('getContents')->andReturn($jsonText);
+
+        GuzzleClient::shouldReceive('request')
             ->withArgs(['GET', config('espinoso.url.holidays')])
             ->andReturn($crawler);
 
-        $text = "Manga de vagos, quedan 16 feriados en todo el año.
+        $text = "Manga de vagos, *quedan 16 feriados* en todo el año.
 
- - Carnaval, inamovible , 12/2 (38)
- - Carnaval, inamovible , 13/2 (39)
- - Día Nacional de la Memoria por la Verdad y la Justicia, inamovible , 24/3 (78)
- - Día del Veterano y de los Caídos en la Guerra de Malvinas, inamovible , 2/4 (87)
- - Día del Trabajador, inamovible , 1/5 (116)
- - Día de la Revolución de Mayo, inamovible , 25/5 (140)
- - Paso a la Inmortalidad del Gral. Don Martín Güemes, nolaborable , 17/6 (163)
- - Paso a la Inmortalidad del General Manuel Belgrano, inamovible , 20/6 (166)
- - Día de la Independencia, inamovible , 9/7 (185)
- - Paso a la Inmortalidad del General José de San Martín, trasladable , 20/8 (227)
- - Día del Respeto a la Diversidad Cultural, trasladable , 15/10 (283)
- - Día de la Soberanía Nacional, trasladable , 19/11 (318)
- - Inmaculada Concepción de María, inamovible , 8/12 (337)
- - Feriado Puente Turístico, puente , 24/12 (353)
- - Navidad, inamovible , 25/12 (354)
- - Feriado Puente Turístico, puente , 31/12 (360)";
+ - *Carnaval*, inamovible, 12/2 (36)
+ - *Carnaval*, inamovible, 13/2 (37)
+ - *Día Nacional de la Memoria por la Verdad y la Justicia*, inamovible, 24/3 (76)
+ - *Día del Veterano y de los Caídos en la Guerra de Malvinas*, inamovible, 2/4 (85)
+ - *Día del Trabajador*, inamovible, 1/5 (114)
+ - *Día de la Revolución de Mayo*, inamovible, 25/5 (138)
+ - *Paso a la Inmortalidad del Gral. Don Martín Güemes*, nolaborable, 17/6 (161)
+ - *Paso a la Inmortalidad del General Manuel Belgrano*, inamovible, 20/6 (164)
+ - *Día de la Independencia*, inamovible, 9/7 (183)
+ - *Paso a la Inmortalidad del General José de San Martín*, trasladable, 20/8 (225)
+ - *Día del Respeto a la Diversidad Cultural*, trasladable, 15/10 (281)
+ - *Día de la Soberanía Nacional*, trasladable, 19/11 (316)
+ - *Inmaculada Concepción de María*, inamovible, 8/12 (335)
+ - *Feriado Puente Turístico*, puente, 24/12 (351)
+ - *Navidad*, inamovible, 25/12 (352)
+ - *Feriado Puente Turístico*, puente, 31/12 (358)";
 
         $this->espinoso->shouldReceive('reply')->once()->with($text);
 
@@ -97,7 +95,7 @@ class NextHolidaysHandlerTest extends HandlersTestCase
         // Act
         $handler->shouldHandle($update);
         $handler->handle($update);
-*/
+
     }
 
     /**
