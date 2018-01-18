18b48cfda1d06957770a52f0b28cf89c46e98722
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2018-01-18 02:12:29 +0000

    min codeclimate issue

--- app/Espinoso/Espinoso.php
+++ app/Espinoso/Espinoso.php
@@ -148,16 +148,16 @@ class Espinoso
         $this->message = $message;
 
         $voice   = $message->getVoice();
-        $file_id = $voice->getFileId();
+        $fileId = $voice->getFileId();
         $stream  = $this->delivery->getVoiceStream($voice);
 
         // Save as ogg (Telegram audio format)
         // and convert it to wav (Voice format required)
-        Storage::put("{$file_id}.ogg", $stream->getContents());
-        $fileIn  = storage_path("app/{$file_id}.ogg");
-        $fileOut = storage_path("app/{$file_id}.wav");
+        Storage::put("{$fileId}.ogg", $stream->getContents());
+        $fileIn  = storage_path("app/{$fileId}.ogg");
+        $fileOut = storage_path("app/{$fileId}.wav");
         @exec("ffmpeg -y -i {$fileIn} {$fileOut} 2> /dev/null");
-        $audio = Storage::get("{$file_id}.wav");
+        $audio = Storage::get("{$fileId}.wav");
         @exec("rm -f {$fileIn} 2> /dev/null");
         @exec("rm -f {$fileOut} 2> /dev/null");
 
