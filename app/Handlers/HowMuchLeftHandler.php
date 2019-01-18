d18cd2e8b8fb5dad8598bd6d22b2d7b0e6e1aee7
marivgil <gil.maricruz@gmail.com>
2019-01-18 00:12:01 -0300

    closed #188

---
+++ app/Handlers/HowMuchLeftHandler.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Handlers;
+
+
+use Carbon\Carbon;
+
+class HowMuchLeftHandler extends MultipleCommand
+{
+
+    protected $ignorePrefix = true;
+
+    protected $signature   = "cuanto falta?";
+    protected $description = "El sueño del Saba";
+
+    protected $patterns = [
+        [
+            'name' => 'saba-day',
+            'pattern' => "cuanto falta unq?"
+        ],[
+            'name' => 'macri-day',
+            'pattern' => "chau macri?"
+        ],[
+            'name' => 'list-days',
+            'pattern' => "cuanto falta?"
+        ],
+        ];
+
+    protected function handleSabaDay(): void
+    {
+        $diff = trans(now()->diffInDays(Carbon::create(now()->year, 03, 18)));
+        $this->espinoso->reply("{$diff} días");
+    }
+
+    public function handleMacriDay(): void
+    {
+        $diff = trans(now()->diffInDays(Carbon::create(now()->year, 12, 10)));
+        $this->espinoso->reply("{$diff} días");
+    }
+
+    public function handleListDays(): void
+    {
+        $diffMacri = trans(now()->diffInDays(Carbon::create(now()->year, 12, 10)));
+        $diffUnq = trans(now()->diffInDays(Carbon::create(now()->year, 03, 18)));
+        $diffGot = trans(now()->diffInDays(Carbon::create(now()->year, 04, 14)));
+        $diffEndGame = trans(now()->diffInDays(Carbon::create(now()->year, 04, 26)));
+        $diffCapMarvel = trans(now()->diffInDays(Carbon::create(now()->year, 03, 8)));
+
+        $list = [
+            'Capitan Marvel' => $diffCapMarvel,
+            'UNQ' => $diffUnq,
+            'GOT' => $diffGot,
+            'EndGame' => $diffEndGame,
+            'Chau Mau' => $diffMacri,
+        ];
+
+        $resultList =[];
+        foreach ($list as $key => $value) {
+            if ($value<=0) {
+                $value = 'Llegoooooo';
+            }
+            array_push($resultList, "{$key}: {$value}");
+        }
+
+        $resultList = collect($resultList)->map(function ($item) {
+            return " {$item}";
+        })->implode("\n");
+
+        $this->espinoso->reply("{$resultList}");
+    }
+}
