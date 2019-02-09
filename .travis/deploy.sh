44725a4ee7982610eb5d9fcd9d39103ca9e5e89b
Leandro <leandro.jdl@gmail.com>
2019-02-09 17:34:12 -0300

    tunning

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     echo "162.243.162.94 ecdsa-sha2-nistp256 $SERVER_KEY" >> ~/.ssh/known_hosts
     ssh forge@162.243.162.94 '/home/forge/espinoso-deploy.sh'
