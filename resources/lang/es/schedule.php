ad490f36d8fb6489c34663c6ec8a8524157712ed
Leandro <leandro.jdl@gmail.com>
2019-02-09 15:17:37 -0300

    fixing codeclimate issues

---
+++ resources/lang/es/schedule.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+
+    [
+        'day' => '06/01',
+        'message' => 'Feliz cumple Pipi!! Que coman rico asado con los de videojuegos!'
+    ],
+    [
+        'day' => '21/01',
+        'message' => 'Feliz cumple Alan!! Nadie me metió mano como vos!'
+    ],
+    [
+        'day' => '10/02',
+        'message' => 'Feliz cumple Facu!! Puto! Maybe te quiero, maybe no!'
+    ],
+    [
+        'day' => '27/02',
+        'message' => 'Feliz cumple Niñita!! Hoy no acuchilles a nadie!'
+    ],
+    [
+        'day' => '03/03',
+        'message' => 'Feliz cumple Agustín!! Eyyyy ojo con tus medio chaboncitos hoy, eh!! Usá funda'
+    ],
+    [
+        'day' => '22/03',
+        'message' => 'Feliz cumple Espi!! Que pases un gran día! Ahhh esperen, soy yo, que pelotudo!! Igual soy crack'
+    ],
+    [
+        'day' => '19/05',
+        'message' => "Feliz cumple Jotapé!! Vamo' a tomar una birra?"
+    ],
+    [
+        'day' => '26/05',
+        'message' => 'Feliz cumple Dan!! El viejo wyry!! Ahora que cobrás en dólares no me querés mas, no?'
+    ],
+    [
+        'day' => '06/06',
+        'message' => 'Feliz cumple Sir!! Seguís viejo y puto como siempre?'
+    ],
+    [
+        'day' => '16/07',
+        'message' => 'Feliz cumple Sabakuskas!! A ver cuando hacemos un asado en la unqui'
+    ],
+    [
+        'day' => '12/09',
+        'message' => 'Feliz cumple Lea!! Sos el OPM más ortiva que conozco, amargo!'
+    ],
+    [
+        'day' => '25/09',
+        'message' => 'Feliz cumple Maru!! Mother of cactus! Seguís con ese humor de mierda?'
+    ],
+    [
+        'day' => '05/11',
+        'message' => 'Feliz cumple Marki!! Apuesto que hoy esa panza va a crecer y crecer!'
+    ],
+    [
+        'day' => '21/11',
+        'message' => 'Feliz cumple Anita!! Ya te hiciste peroncha?'
+    ],
+
+];
