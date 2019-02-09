73a96aa14b02a06ab81714446c959b45b518fe54
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2019-02-09 01:28:01 -0300

    ultima prubea

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,7 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     echo "162.243.162.94 ecdsa-sha2-nistp256 $SERVER_KEY" >> ~/.ssh/known_hosts
-    ssh deploy@162.243.162.94 '/home/forge/espinoso-deploy.sh'
+    ssh forge@162.243.162.94 '/home/forge/espinoso-deploy.sh'
 else
     echo "Not deploying, since this branch isn't master."
 fi
