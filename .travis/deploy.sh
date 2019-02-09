9d3d55421cf3e86442d10df6e20dc08966f3acbe
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2019-02-09 01:12:19 -0300

    soy un pelotudo

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,7 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     echo "162.243.162.94 ecdsa-sha2-nistp256 $SERVER_KEY" >> ~/.ssh/known_hosts
-    ssh deploy@162.243.162.94 '/home/forge/espinoso-deploy-sh'
+    ssh deploy@162.243.162.94 '/home/forge/espinoso-deploy.sh'
 #    git remote add deploy "deploy@162.243.162.94:/home/forge/repos/espinoso"
 #    git push deploy master
 else
